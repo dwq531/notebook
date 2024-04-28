b74234e42ab651b1675c2009ac812d22a84ad47c
dwq531 <duwanqingdwq@163.com>
2024-04-28 11:52:27 +0800

    feat:编辑文本界面跳转

---
+++ app/src/main/java/com/example/notebook/text_editor.java
@@ -0,0 +1,13 @@
+package com.example.notebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class text_editor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_editor);
+    }
+}
--- app/src/main/java/com/example/notebook/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/notebook/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,27 @@
 package com.example.notebook.ui.dashboard;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.notebook.R;
 import com.example.notebook.databinding.FragmentDashboardBinding;
+import com.example.notebook.text_editor;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    Button addButton,searchButton;
+    private final int ADD_NOTE = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,10 +30,27 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        addButton = root.findViewById(R.id.but_add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 跳转到新建编辑文本页面
+                Intent intent = new Intent(getActivity(), text_editor.class);
+                startActivityForResult(intent,ADD_NOTE);
+            }
+        });
         return root;
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_NOTE) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    String resultValue = data.getStringExtra("key");
+                    // TODO
+                }
+            }
+        }
     }
 
     @Override
